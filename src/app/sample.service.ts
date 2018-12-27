f6db1704b6ec98a768ef345b4e8d0aa1c08a459d
y-kaino <yoppy1125yoppy@gmail.com>
2018-12-27 15:07:48 +0900

    [Update] Getの追加

--- src/app/sample.service.ts
+++ src/app/sample.service.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -6,4 +6,13 @@ export class SampleService {
 
   constructor(private http: HttpClient) { }
 
+  get(): Promise<any>{
+    const url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040";
+    const myParameter = "MyParametorValue";
+
+    return this.http.get(url,{
+      
+      params: new HttpParams().set("myParameter",myParameter)
+    }).toPromise();
+  }
 }
